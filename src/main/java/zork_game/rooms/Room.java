c404025608412fd1d24670b2a15ca186a7f63d70
Supakrit <supakrit.nuc@student.mahidol.edu>
2020-05-23 14:33:23 +0700

    Modify Rooms

--- src/main/java/zork_game/rooms/Room.java
+++ src/main/java/zork_game/rooms/Room.java
@@ -1,12 +1,51 @@
 package zork_game.rooms;
 
 import zork_game.items.Item;
+import zork_game.monsters.Monster;
 
-public abstract class Room {
+public class Room {
 
     public Room northRoom;
     public Room southRoom;
     public Room westRoom;
     public Room eastRoom;
     public Item item;
+    public Monster monster;
+
+    public Room(){
+        northRoom = null;
+        southRoom = null;
+        eastRoom = null;
+        westRoom = null;
+        item = null;
+        monster = null;
+    }
+
+    public void setNorthRoom(Room northRoom) {
+        this.northRoom = northRoom;
+        northRoom.southRoom = this;
+    }
+
+    public void setSouthRoom(Room southRoom) {
+        this.southRoom = southRoom;
+        southRoom.northRoom = this;
+    }
+
+    public void setWestRoom(Room westRoom) {
+        this.westRoom = westRoom;
+        westRoom.eastRoom = this;
+    }
+
+    public void setEastRoom(Room eastRoom) {
+        this.eastRoom = eastRoom;
+        eastRoom.westRoom = this;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public void setMonster(Monster monster) {
+        this.monster = monster;
+    }
 }
--- src/main/java/zork_game/rooms/StartingRoom.java
+++ src/main/java/zork_game/rooms/StartingRoom.java
@@ -2,11 +2,4 @@ package zork_game.rooms;
 
 public class StartingRoom extends Room {
 
-    public StartingRoom(){
-        northRoom = null;
-        southRoom = null;
-        eastRoom = null;
-        westRoom = null;
-        item = null;
-    }
 }
